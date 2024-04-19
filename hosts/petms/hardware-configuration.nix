dc46f0c11483bed96a2ef336dd17693f68505064
Peter Marshall <petms@proton.me>
2024-04-18 20:26:57 -0400

    Fix bcachefs mount

--- hosts/petms/hardware-configuration.nix
+++ hosts/petms/hardware-configuration.nix
@@ -1,12 +1,12 @@
 { modulesPath, ... }: {
 
-  boot.initrd.kernelModules = [ "usb_storage" "uas" "ahci" "xhci_pci" "ehci_pci" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "usb_storage" "uas" "ahci" "xhci_pci" "ehci_pci" "sd_mod" "rtsx_pci_sdmmc" ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-partlabel/nixos1:/dev/disk/by-partlabel/nixos2";
+      device = "UUID=e868fc43-6591-420d-be5b-66574052ae82";
       fsType = "bcachefs";
       options = [ "compression=zstd" ];
     };
