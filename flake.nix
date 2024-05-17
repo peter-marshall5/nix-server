34c34db4589b490a6a55f7ac658dd7c63892dce6
Peter Marshall <petms@proton.me>
2024-05-17 14:22:34 -0400

    Remove minecraft server's double container

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
   };
-  outputs = { self, nixpkgs, deploy-rs, agenix }: {
+  outputs = { self, nixpkgs, agenix }: {
     nixosConfigurations.petms = nixpkgs.lib.nixosSystem {
       modules = [
         ./modules
