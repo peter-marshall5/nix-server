a3f95405a86db2bc777ffa03ab4d04dea654a733
Peter Marshall <petms@proton.me>
2024-05-19 14:39:08 -0400

    Enable user lingering

--- hosts/cheesecake/users.nix
+++ hosts/cheesecake/users.nix
@@ -10,6 +10,7 @@
 
   petms = {
     isNormalUser = true;
+    linger = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjg1Y1b2YyhoC73I4is0/NRmVb3FeRmpLf2Yk8adrxq petms@peter-pc"
     ];
@@ -17,6 +18,7 @@
 
   jomshall = {
     isNormalUser = true;
+    linger = true;
   };
 
 }
